3cc8759eef49669207b3985e899758287f849c86
yzhq97 <yzhq97@buaa.edu.cn>
2017-12-28 16:15:22 +0800

    UI changes

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -2,14 +2,17 @@
 
 @section('default_content')
     <div class="container">
-        <div class="col-md-4 col-md-offset-4"
-             style="border-radius: 5px; box-shadow: 0 2px 7px #bbbbbb; background-color: white; padding: 0;">
-            <div class="bg-primary text-center"
-                 style="border-radius: 5px; padding-top: 20px; padding-bottom: 15px; box-shadow: 0 2px 7px #bbbbbb;">
-                <h3 style="color: white; font-weight: normal;">{{ config('app.name') }}</h3>
-            </div>
-            <div style="padding: 10px">
-                @yield('content')
+        <div class="row" style="height: 60px;"></div>
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4"
+                 style="border-radius: 5px; box-shadow: 0 2px 7px #bbbbbb; background-color: white; padding: 0;">
+                <div class="bg-primary text-center"
+                     style="border-radius: 5px; padding-top: 5px; padding-bottom: 15px; box-shadow: 0 2px 7px #bbbbbb;">
+                    <h3 style="color: white; font-weight: normal;">{{ config('app.name') }}</h3>
+                </div>
+                <div style="padding: 20px">
+                    @yield('content')
+                </div>
             </div>
         </div>
     </div>
